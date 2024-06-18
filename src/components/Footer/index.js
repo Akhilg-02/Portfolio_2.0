620589dab78a106c95e1a1509b9154266e6019de
unknown <guptaakhil0206@gmail.com>
2024-06-18 13:03:02 +0530

    added info. for projects and experience

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Akhil Gupta</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
